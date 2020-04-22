156e541f951a8ee7cc180480739caa8d1c287de6
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-04-22 17:24:27 -0400

    setting up routers

--- api/server.js
+++ api/server.js
@@ -2,8 +2,31 @@ const express = require("express");
 
 const db = require("../data/dbConfig.js");
 
+const welcome_router= require('../routers/welcome-router.js');
+
 const server = express();
 
+// built-in middleware
 server.use(express.json());
+// third party middleware
+server.use(helmet());
+
+// route handlers
+server.use(welcome_router);
+
+// when there is no router, handler
+server.use((req, res) => {
+    res.status(404).send(
+        `<h4 align='center'>The url ${req.url.toUpperCase()} was not found.</h4>`
+    );
+});
+
+// error, handler
+server.use((err, req, res, next) => {
+    console.log('Server error:', err)
+	res.status(500).json({
+		message: "Oops, something went wrong. Please try again later.",
+	});
+});
 
 module.exports = server;
